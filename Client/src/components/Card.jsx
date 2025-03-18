834e9d02455fa88c821425f0c110e4ca1beae203
Samind2 <suphaneerangsirat@gmail.com>
2025-03-18 09:39:29 +0700

    upLandingPage

---
+++ Client/src/components/Card.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+
+const Card = () => {
+//   return (
+//     <div className="flex justify-center gap-8 py-10">
+//       {/* เพิ่มการ์ดที่หนึ่ง */}
+//       <div className="card card-side bg-base-100 shadow-lg border border-gray-300 w-80">
+//       <figure className="w-full">
+//           <img
+//             src="/images/Home/Vaccine.png"
+//             alt="Movie"
+//             className="w-full h-48 object-cover" // ทำให้รูปเต็มช่องการ์ด
+//           />
+//         </figure>
+//         <div className="card-body">
+//           <h3 className="md:text-xl font-bold md:leading-snug leading-snug">
+//             <span className="text-[#E51317]">บันทึกข้อมูลวัคซีน</span>
+//           </h3>
+//           <p>Click the button to watch on Jetflix app.</p>
+//           <div className="flex justify-center">
+//             <a
+//               className="btn bg-[#E51317] px-8 py-3 font-semibold text-white rounded-full"
+//               href="/shop"
+//             >
+//               ลงทะเบียน
+//             </a>
+//           </div>
+//         </div>
+//       </div>
+
+//       {/* เพิ่มการ์ดที่สอง */}
+//       <div className="card card-side bg-base-100 shadow-lg border border-gray-300 w-80">
+//         <figure className="w-full">
+//           <img
+//             src="/images/Home/ChildDevelop2.png"
+//             alt="Movie"
+//             className="w-full h-48 object-cover" // ทำให้รูปเต็มช่องการ์ด
+//           />
+//         </figure>
+//         <div className="card-body">
+//         <h3 className="md:text-xl font-bold md:leading-snug leading-snug">
+//             <span className="text-[#114965]">บันทึกข้อมูลพัฒนาการ</span>
+//           </h3>
+//           <p>Click the button to watch on Jetflix app.</p>
+//           <div className="flex justify-center">
+//             <a
+//               className="btn bg-[#E51317] px-8 py-3 font-semibold text-white rounded-full"
+//               href="/shop"
+//             >
+//               ลงทะเบียน
+//             </a>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// 
+};
+
+export default Card;
---
+++ Client/src/pages/Home/Index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import HeroSection from './HeroSection'
+import Card from '../../components/Card'
+import InformationRecord from './InformationRecord'
+
+const Index = () => {
+ return (
+  <div>
+   <HeroSection />
+   <InformationRecord />
+   <Card />
+  </div>
+ )
+}
+
+export default Index
