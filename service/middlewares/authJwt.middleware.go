2957409c44ff9db792d61f0305dfdfefb6b96b69
Plashon <fourza12poobate@gmail.com>
2025-03-23 23:52:12 +0700

    token

---
+++ service/middlewares/authJwt.middleware.go
@@ -0,0 +1,52 @@
+package middleware
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+// VerifyToken เป็น Middleware สำหรับตรวจสอบ JWT
+// gin.HandlerFunc สร้าง middleware
+func VerifyToken() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// ดึง Token จาก Header
+		tokenString := c.GetHeader("x-access-token")
+		if tokenString == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"message": "Token missing"})
+			c.Abort()
+			return
+		}
+
+		// Secret Key จาก .env
+		secretKey := os.Getenv("KEY_PASS")
+		if secretKey == "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"message": "Missing secret key"})
+			c.Abort()
+			return
+		}
+
+		// ตรวจสอบ Token
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			return []byte(secretKey), nil
+		})
+
+		// ถ้า Token ไม่ถูกต้อง
+		if err != nil || !token.Valid {
+			c.JSON(http.StatusForbidden, gin.H{"message": "Access Forbidden"})
+			c.Abort()
+			return
+		}
+
+		// ดึง Claims (ข้อมูลที่อยู่ใน Token)
+		if claims, ok := token.Claims.(jwt.MapClaims); ok {
+			c.Set("userId", claims["id"])
+			c.Set("username", claims["username"])
+		}
+
+		// ไปยังฟังก์ชันถัดไป
+		c.Next()
+	}
+}
